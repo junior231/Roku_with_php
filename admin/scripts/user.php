e731be8f157828b2acf532297d4383f527a2aff7
Ilo <c_ilo@ats-hr420-dm31.fc.ca>
2020-03-11 10:38:14 -0400

    routing all the way to the userHome

--- admin/scripts/user.php
+++ admin/scripts/user.php
@@ -49,12 +49,32 @@ function getAllUsers(){
     $pdo = Database::getInstance()->getConnection();
 
     $get_user_query = 'SELECT * FROM tbl_user';
-    $users = $pdo->query($get_user_query);
+    $get_user_set = $pdo->prepare($get_user_query);
+    $get_user_result = $get_user_set->execute();
 
-    if($users){
-        return $users;
-    }else{
-        return false;
+    $users = array();
+
+    if($get_user_result) {
+        while($user = $get_user_set->fetch(PDO::FETCH_ASSOC)) {
+            // parse all the users info like we did for a single user and pass
+            // then into the array
+
+            $currentuser = array();
+
+            $currentuser['id'] = $user['user_id'];
+            $currentuser['admin'] = $user['user_admin'];
+            $currentuser['avatar'] = $user['user_avatar'];
+            $currentuser['permissions'] = $user['user_permissions'];
+            $currentuser['uname'] = $user['user_name'];
+
+
+            $users[] = $currentuser;
+
+        }
+
+        return json_encode($users);
+    } else {
+        return 'There was a problem getting the users';
     }
 }
 
